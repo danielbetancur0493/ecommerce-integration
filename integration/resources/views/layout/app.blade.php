a97906fa51fb75f0d17f20f4319d2982f335a536
danielbetancur <danielbetancur0493@gmail.com>
2018-09-23 00:01:56 -0500

    AJUSTES LIBERACIÓN DE ARCHIVOS

--- integration/resources/views/layout/app.blade.php
+++ integration/resources/views/layout/app.blade.php
@@ -3,10 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>@yield('title', 'Default') </title>
+	<link rel="shortcut icon" href="{{asset('img/ecommerce.png')}}" />
 	<link rel="stylesheet" type="text/css" href="{{asset('plugins/bootstrap/css/bootstrap.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('plugins/chosen/chosen.css')}}">
-	<link rel="stylesheet" href="{{asset('plugins/trumbowyg/dist/ui/trumbowyg.css')}}">
-	<link rel="stylesheet" href="{{asset('plugins/fileinput/css/fileinput.css')}}">
 	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
@@ -30,9 +28,8 @@
 
 	<script src="{{asset('plugins/jquery/jquery-3.2.1.js')}}"></script>
 	<script src="{{asset('plugins/bootstrap/js/bootstrap.min.js')}}"></script>
-	<script src="{{asset('plugins/chosen/chosen.jquery.js')}}"></script>
-	<script src="{{ asset('plugins/trumbowyg/dist/trumbowyg.min.js')}}"></script>
-	<script src="{{asset('plugins/fileinput/js/fileinput.js')}}"></script>
+	
+	
 	@yield('js')
 	
 </body>
